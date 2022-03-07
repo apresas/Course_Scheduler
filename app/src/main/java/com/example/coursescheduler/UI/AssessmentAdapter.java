b9d86bc9a8514fe6fa55d2e4aa75bfc9dd74c80f
apresas <a.presas@outlook.com>
2022-03-07 11:27:08 -0800

    10th Commit

---
+++ app/src/main/java/com/example/coursescheduler/UI/AssessmentAdapter.java
@@ -0,0 +1,93 @@
+package com.example.coursescheduler.UI;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.coursescheduler.Entity.Assessment;
+import com.example.coursescheduler.Entity.Course;
+import com.example.coursescheduler.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AssessmentAdapter extends RecyclerView.Adapter<AssessmentAdapter.AssessmentHolder> {
+    private List<Assessment> assessments = new ArrayList();
+    private OnItemClickListener listener;
+
+    @NonNull
+    @Override
+    public AssessmentHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.assessment_item, parent, false);
+        return new AssessmentHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AssessmentHolder holder, int position) {
+        Assessment currentAssessment = assessments.get(position);
+        int ID = currentAssessment.getAssessmentID();
+
+        holder.assessmentIDTextView.setText(Integer.toString(ID));
+        holder.textViewTitle.setText(currentAssessment.getAssessmentTitle());
+        holder.textViewType.setText(currentAssessment.getAssessmentType());
+        holder.textViewStart.setText(currentAssessment.getStartDate());
+        holder.textViewEnd.setText(currentAssessment.getEndDate());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return assessments.size();
+    }
+
+    public void setAssessments(List<Assessment> assessments) {
+        this.assessments = assessments;
+        notifyDataSetChanged();
+    }
+
+    public Assessment getAssessmentAt(int position) {
+        return assessments.get(position);
+    }
+
+    class AssessmentHolder extends RecyclerView.ViewHolder {
+
+        private TextView assessmentIDTextView;
+        private TextView textViewTitle;
+        private TextView textViewType;
+        private TextView textViewStart;
+        private TextView textViewEnd;
+
+        public AssessmentHolder(@NonNull View itemView) {
+            super(itemView);
+            assessmentIDTextView = itemView.findViewById(R.id.text_view_assessmentID);
+            textViewTitle = itemView.findViewById(R.id.text_view_course_title);
+            textViewType = itemView.findViewById(R.id.text_view_type);
+            textViewStart = itemView.findViewById(R.id.text_view_assessment_start);
+            textViewEnd = itemView.findViewById(R.id.text_view_assessment_end);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    if (listener != null && position != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(assessments.get(position));
+                    }
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Assessment assessment);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+}
---
+++ app/src/main/java/com/example/coursescheduler/UI/AssessmentViewModel.java
@@ -0,0 +1,45 @@
+package com.example.coursescheduler.UI;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.coursescheduler.Database.ScheduleRepo;
+import com.example.coursescheduler.Entity.Assessment;
+import com.example.coursescheduler.Entity.Course;
+
+import java.util.List;
+
+public class AssessmentViewModel extends AndroidViewModel {
+
+    private ScheduleRepo repository;
+    private LiveData<List<Assessment>> allAssessments;
+
+    public AssessmentViewModel(@NonNull Application application) {
+        super(application);
+        repository = new ScheduleRepo(application);
+        allAssessments = repository.getAllAssessments();
+    }
+
+    public void insert(Assessment assessment) {
+        repository.insertAssessment(assessment);
+    }
+
+    public void update(Assessment assessment) {
+        repository.updateAssessment(assessment);
+    }
+
+    public void delete(Assessment assessment) {
+        repository.deleteAssessment(assessment);
+    }
+
+    public void deleteAllAssessments() {
+        repository.deleteAllAssessments();
+    }
+
+    public LiveData<List<Assessment>> getAllAssessments() {
+        return allAssessments;
+    }
+}
